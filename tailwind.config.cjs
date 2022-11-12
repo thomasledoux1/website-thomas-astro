eae207e56efd365133236afcf1753c7223ffeda3
Thomas Ledoux <tledoux@reference.be>
2022-11-12 16:50:02 +0100

    Cleanup

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,6 @@ module.exports = {
     './src/layouts/**/*.{js,ts,jsx,tsx,astro}',
     './src/components/**/*.{js,ts,jsx,tsx,astro}',
   ],
-  darkMode: 'class',
   theme: {
     container: {
       padding: {
@@ -24,28 +23,10 @@ module.exports = {
         primary: 'var(--primary)',
         secondary: 'var(--secondary)',
       },
-      inset: {
-        timelineCircle: 'calc(50% - 0.5em)',
-      },
-      boxShadow: {
-        case: '0 1px 3px rgba(0,0,0,.12), 0 1px 2px rgba(0,0,0,.24)',
-        'case-hover': '0 10px 28px rgba(0,0,0,.25), 0 8px 10px rgba(0,0,0,.22)',
-      },
-      rotate: {
-        135: '135deg',
-        '-135': '-135deg',
-      },
-      zIndex: {
-        '-1': '-1',
-      },
       fontFamily: {
         heading: ['Inter', ...defaultTheme.fontFamily.sans],
         body: ['Inter', ...defaultTheme.fontFamily.sans],
       },
     },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
 };
