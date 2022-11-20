0ae5e338eed18033d93545d46e64b0f5fee27357
Thomas Ledoux <tledoux@reference.be>
2022-11-20 10:58:14 +0100

    Fix local development

--- src/lib/prisma-client.ts
+++ src/lib/prisma-client.ts
@@ -1,5 +1,9 @@
-import { PrismaClient } from '@prisma/client/edge';
+import { PrismaClient as PrismaClientEdge } from '@prisma/client/edge';
+import { PrismaClient as PrismaClientDev } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const prisma =
+  process.env.NODE_ENV === 'development'
+    ? new PrismaClientDev()
+    : new PrismaClientEdge();
 
 export { prisma };
