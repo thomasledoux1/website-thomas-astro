580eb03dbc05d7c09dd5943ade8a09a02216af2f
Thomas Ledoux <tledoux@reference.be>
2022-12-11 11:06:11 +0100

    Adding e-mail on comment

--- src/lib/get-comments-for-blog.ts
+++ src/lib/get-comments-for-blog.ts
@@ -1,18 +1,11 @@
-import { prisma } from "./prisma-client";
+import { prisma } from './prisma-client';
 
 export const getCommentsForBlog = async (blogUrl: string | null) => {
   const comments = await prisma.post.findFirst({
     where: { url: blogUrl ?? undefined },
     include: { Comment: true },
-    orderBy: { createdAt: "asc" },
+    orderBy: { createdAt: 'asc' },
   });
-  // const comments = await supabase
-  //   .from('comments')
-  //   .select('*, blogs(*)')
-  //   .eq('blogs.url', blogUrl)
-  //   .order('created_at', {
-  //     ascending: false,
-  //   });
   const allCommentsInDbForPost = comments?.Comment;
   return allCommentsInDbForPost;
 };
--- src/pages/api/comments/create.ts
+++ src/pages/api/comments/create.ts
@@ -1,5 +1,40 @@
 import type { APIRoute } from 'astro';
+import nodemailer from 'nodemailer';
 import { prisma } from '../../../lib/prisma-client';
+import mjml2html from 'mjml';
+
+let transporter = nodemailer.createTransport({
+  host: 'smtp.sendgrid.net',
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: 'apikey',
+    pass: process.env.SENDGRID_API_KEY,
+  },
+});
+
+async function sendMail(blogUrl: string, author: string, comment: string) {
+  const htmlOutput = mjml2html(`
+    <mjml>
+      <mj-body>
+        <mj-section>
+          <mj-column>
+            <mj-text>
+              A new comment was added to blog ${blogUrl} by ${author}:
+              ${comment}
+            </mj-text>
+          </mj-column>
+        </mj-section>
+      </mj-body>
+    </mjml>`);
+
+  await transporter.sendMail({
+    from: '"Thomas Ledoux" <info@thomasledoux.be>',
+    to: process.env.EMAIL_TO,
+    subject: `New comment on post ${blogUrl}`,
+    html: htmlOutput.html,
+  });
+}
 
 export const post: APIRoute = async ({ request }) => {
   const formData = await request.formData();
@@ -20,12 +55,17 @@ export const post: APIRoute = async ({ request }) => {
             url: blogUrl as string,
           },
           where: {
-            id: blog?.id,
+            id: blog?.id ?? 0,
           },
         },
       },
     },
   });
+  try {
+    sendMail(blogUrl as string, author as string, comment as string);
+  } catch (err) {
+    console.error('Error sending email', err);
+  }
 
   return new Response(null, {
     status: 200,
