8bf97c0a1b7bc2c1ca7f516b3d602c7a133d9cfb
Thomas Ledoux <tledoux@reference.be>
2022-12-29 13:13:50 +0100

    Another attempt to fix env vars

--- src/pages/custom-api/comments.ts
+++ src/pages/custom-api/comments.ts
@@ -1,6 +1,61 @@
 import type { APIRoute } from 'astro';
-import { createComment, getComments } from '../../lib/comments';
-import { sendMail } from '../../lib/emails';
+import type { Comment } from '@prisma/client';
+import type { PrismaClient } from '@prisma/client/index.js';
+
+let prisma: PrismaClient | undefined;
+console.log(import.meta.env);
+
+if (import.meta.env.MODE === 'development') {
+  import('@prisma/client/index.js').then(
+    mod => (prisma = new mod.PrismaClient())
+  );
+} else {
+  import('@prisma/client/edge.js').then(
+    mod =>
+      (prisma = new mod.PrismaClient({
+        datasources: {
+          db: {
+            url: import.meta.env.DATABASE_URL,
+          },
+        },
+      }))
+  );
+}
+
+const sendMail = async (
+  subject: string,
+  content: { type: string; value: string }[]
+) => {
+  fetch('https://api.sendgrid.com/v3/mail/send', {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${import.meta.env.SENDGRID_API_KEY}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      personalizations: [
+        {
+          to: [
+            {
+              email: import.meta.env.EMAIL_TO,
+              name: 'Thomas Ledoux',
+            },
+          ],
+        },
+      ],
+      from: {
+        email: 'info@thomasledoux.be',
+        name: 'Thomas Ledoux',
+      },
+      replyTo: {
+        email: 'info@thomasledoux.be',
+        name: 'Thomas Ledoux',
+      },
+      subject,
+      content,
+    }),
+  });
+};
 
 export const get: APIRoute = async ({ request }) => {
   const url = new URL(request.url);
@@ -11,8 +66,13 @@ export const get: APIRoute = async ({ request }) => {
       status: 400,
     });
   }
-  const comments = await getComments(blogUrl);
-  return new Response(JSON.stringify(comments), {
+  const comments = await prisma?.post.findFirst({
+    where: { url: (blogUrl as string | undefined) ?? undefined },
+    include: { Comment: true },
+    orderBy: { createdAt: 'asc' },
+  });
+  const allCommentsInDbForPost = comments?.Comment;
+  return new Response(JSON.stringify(allCommentsInDbForPost), {
     status: 200,
     headers: {
       'Content-Type': 'application/json',
@@ -23,7 +83,30 @@ export const get: APIRoute = async ({ request }) => {
 export const post: APIRoute = async ({ request }) => {
   const body = await request.json();
   const { comment, author, blogUrl } = body;
-  const commentInDb = await createComment(author, blogUrl, comment);
+  let commentInDb: Comment | undefined;
+  const blog = await prisma?.post.findFirst({
+    where: { url: blogUrl },
+  });
+  try {
+    commentInDb = await prisma?.comment.create({
+      data: {
+        author: author ?? '',
+        text: comment ?? '',
+        post: {
+          connectOrCreate: {
+            create: {
+              url: blogUrl ?? '',
+            },
+            where: {
+              id: blog?.id ?? 0,
+            },
+          },
+        },
+      },
+    });
+  } catch (err) {
+    console.error('Error saving comment', err);
+  }
   if (!commentInDb) {
     return new Response(null, { status: 400 });
   }
