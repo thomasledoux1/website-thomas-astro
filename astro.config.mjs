30208746f58cd897616bf0269b8534548b1a1d33
Thomas Ledoux <tledoux@reference.be>
2022-12-30 10:59:51 +0100

    Adding prefetch + rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
-import { remarkReadingTime } from "./src/utils/calculate-reading-time.mjs";
-import vercel from "@astrojs/vercel/edge";
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import { remarkReadingTime } from './src/utils/calculate-reading-time.mjs';
+import vercel from '@astrojs/vercel/edge';
+import react from '@astrojs/react';
+import prefetch from '@astrojs/prefetch';
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,11 +16,13 @@ export default defineConfig({
     }),
     react(),
     mdx(),
+    prefetch(),
   ],
-  output: "server",
+  output: 'server',
   adapter: vercel(),
   markdown: {
     remarkPlugins: [remarkReadingTime],
     extendDefaultPlugins: true,
   },
+  site: 'https://www.thomasledoux.be',
 });
