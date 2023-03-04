7a3bce4f922fbeb2426031046bc8ec53e1fa688a
Thomas Ledoux <thomas_ledoux@epam.com>
2023-03-04 14:37:42 +0100

    Optimize images

--- src/utils/calculate-blurhash-gradient.ts
+++ src/utils/calculate-blurhash-gradient.ts
@@ -1,15 +1,15 @@
-import { encode } from "blurhash";
-import { getPixels } from "@unpic/pixels";
-import { blurhashToCssGradientString } from "@unpic/placeholder";
+import { encode } from 'blurhash';
+import { getPixels } from '@unpic/pixels';
+import { blurhashToCssGradientString } from '@unpic/placeholder';
 
 const calculateBlurhashGradient = async (url: string) => {
   const jpgData = await getPixels(
-    url
+    url.replace('/upload', '/upload/w_100,q_auto,f_jpg,e_blur:1000')
   );
   const data = Uint8ClampedArray.from(jpgData.data);
   const blurhash = encode(data, jpgData.width, jpgData.height, 4, 4);
   const cssGradient = blurhashToCssGradientString(blurhash);
-  return cssGradient
-}
+  return cssGradient;
+};
 
-export {calculateBlurhashGradient}
+export { calculateBlurhashGradient };
