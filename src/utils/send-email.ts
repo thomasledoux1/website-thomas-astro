cff0b746465ab589e2e8cad63505a8cab31e90a8
Thomas Ledoux <thomas_ledoux@epam.com>
2023-03-08 10:14:42 +0100

    Add debugging for email

--- src/utils/send-email.ts
+++ src/utils/send-email.ts
@@ -2,33 +2,39 @@ export const sendMail = async (
   subject: string,
   content: { type: string; value: string }[]
 ) => {
-  fetch('https://api.sendgrid.com/v3/mail/send', {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${import.meta.env.SENDGRID_API_KEY}`,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      personalizations: [
-        {
-          to: [
-            {
-              email: import.meta.env.EMAIL_TO,
-              name: 'Thomas Ledoux',
-            },
-          ],
+  try {
+    fetch('https://api.sendgrid.com/v3/mail/send', {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${import.meta.env.SENDGRID_API_KEY}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        personalizations: [
+          {
+            to: [
+              {
+                email: import.meta.env.EMAIL_TO,
+                name: 'Thomas Ledoux',
+              },
+            ],
+          },
+        ],
+        from: {
+          email: 'info@thomasledoux.be',
+          name: 'Thomas Ledoux',
         },
-      ],
-      from: {
-        email: 'info@thomasledoux.be',
-        name: 'Thomas Ledoux',
-      },
-      replyTo: {
-        email: 'info@thomasledoux.be',
-        name: 'Thomas Ledoux',
-      },
-      subject,
-      content,
-    }),
-  });
+        replyTo: {
+          email: 'info@thomasledoux.be',
+          name: 'Thomas Ledoux',
+        },
+        subject,
+        content,
+      }),
+    }).then(res => {
+      console.log(res);
+    });
+  } catch (e) {
+    console.error('something went wrong while sending email', e);
+  }
 };
