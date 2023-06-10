8998179929efbd8cd8def259ed74b32f2edb24f6
Thomas Ledoux <thomas_ledoux@epam.com>
2023-06-10 09:59:24 +0200

    Upgrading deps + better collections handling

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,9 +3,8 @@ import { z, defineCollection } from 'astro:content';
 const blog = defineCollection({
   schema: z.object({
     title: z.string(),
-    layout: z.string(),
     tags: z.array(z.string()),
-    date: z.string(),
+    date: z.string().datetime(),
     image: z.string(),
     imageAlt: z.string(),
   }),
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,18 +1,20 @@
 import rss from '@astrojs/rss';
-const postImportResult = import.meta.glob('./blog/*.mdx', { eager: true });
-const posts = Object.values(postImportResult);
+import { getCollection } from 'astro:content';
 
-export const get = () =>
-  rss({
+export const get = async () => {
+  const blogs = await getCollection('blog');
+
+  return rss({
     title: 'Blog by Thomas Ledoux',
     description:
       'Blog by Thomas Ledoux. Writes about Javascript/React/Next.js/Remix/Astro. Always looking to learn about the newest frameworks and features.',
     site: import.meta.env.SITE,
-    items: posts.map(post => ({
-      link: post.url,
-      title: post.frontmatter.title,
-      pubDate: post.frontmatter.date,
+    items: blogs.map(post => ({
+      link: `/blog/${post.slug}`,
+      title: post.data.title,
+      pubDate: post.data.date,
     })),
     customData: `<language>en-us</language>`,
     stylesheet: '/rss/styles.xsl',
   });
+};
