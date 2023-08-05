d55eb761bb4f2c7caafe61b5606c610c22fe8b27
Thomas Ledoux <thomas_ledoux@epam.com>
2023-08-05 10:00:19 +0200

    Change deploy to netlify

--- astro.config.ts
+++ astro.config.ts
@@ -2,47 +2,37 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import { remarkReadingTime } from "./src/utils/calculate-reading-time.js";
-import vercel from "@astrojs/vercel/serverless";
+import {netlifyFunctions} from "@astrojs/netlify";
 import react from "@astrojs/react";
 import prefetch from "@astrojs/prefetch";
 import AutoImport from "astro-auto-import";
-import {
-  astroCodeSnippets,
-  codeSnippetAutoImport,
-} from "./integrations/astro-code-snippets";
+import { astroCodeSnippets, codeSnippetAutoImport } from "./integrations/astro-code-snippets";
 import partytown from "@astrojs/partytown";
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
-    AutoImport({
-      imports: [codeSnippetAutoImport],
-    }),
-    astroCodeSnippets(),
-    prefetch(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-    mdx(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), react(), AutoImport({
+    imports: [codeSnippetAutoImport]
+  }), astroCodeSnippets(), prefetch(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), mdx()],
   vite: {
     define: {
-      "process.env.NODE_ENV": `'${process.env.NODE_ENV}'`,
-    },
+      "process.env.NODE_ENV": `'${process.env.NODE_ENV}'`
+    }
   },
   output: "hybrid",
-  adapter: vercel(),
+  adapter: netlifyFunctions(),
   markdown: {
-    remarkPlugins: [remarkReadingTime],
+    remarkPlugins: [remarkReadingTime]
   },
   experimental: {
-    viewTransitions: true,
+    viewTransitions: true
   },
-  site: "https://www.thomasledoux.be",
+  site: "https://www.thomasledoux.be"
 });
