1ae8d2f90f0e1958a33f36b74bd9084395102fb9
Thomas Ledoux <thomas_ledoux@epam.com>
2023-09-14 20:57:07 +0200

    Fix build errs

--- astro.config.ts
+++ astro.config.ts
@@ -2,35 +2,45 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import { remarkReadingTime } from "./src/utils/calculate-reading-time.js";
-import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
 import prefetch from "@astrojs/prefetch";
 import AutoImport from "astro-auto-import";
-import { astroCodeSnippets, codeSnippetAutoImport } from "./integrations/astro-code-snippets";
+import {
+  astroCodeSnippets,
+  codeSnippetAutoImport,
+} from "./integrations/astro-code-snippets";
 import partytown from "@astrojs/partytown";
 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), react(), AutoImport({
-    imports: [codeSnippetAutoImport]
-  }), astroCodeSnippets(), prefetch(), partytown({
-    config: {
-      forward: ["dataLayer.push"]
-    }
-  }), mdx()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+    AutoImport({
+      imports: [codeSnippetAutoImport],
+    }),
+    astroCodeSnippets(),
+    prefetch(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    mdx(),
+  ],
   vite: {
     define: {
-      "process.env.NODE_ENV": `'${process.env.NODE_ENV}'`
-    }
+      "process.env.NODE_ENV": `'${process.env.NODE_ENV}'`,
+    },
   },
   output: "hybrid",
   adapter: cloudflare(),
   markdown: {
-    remarkPlugins: [remarkReadingTime]
+    remarkPlugins: [remarkReadingTime],
   },
-  site: "https://www.thomasledoux.be"
+  site: "https://www.thomasledoux.be",
 });
