826e248c706304cfd7b9c4bb5406b580ec00a918
Thomas Ledoux <thomas_ledoux@epam.com>
2023-09-14 20:52:23 +0200

    Cloudflare

--- astro.config.ts
+++ astro.config.ts
@@ -6,42 +6,31 @@ import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
 import prefetch from "@astrojs/prefetch";
 import AutoImport from "astro-auto-import";
-import {
-  astroCodeSnippets,
-  codeSnippetAutoImport,
-} from "./integrations/astro-code-snippets";
+import { astroCodeSnippets, codeSnippetAutoImport } from "./integrations/astro-code-snippets";
 import partytown from "@astrojs/partytown";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
-    AutoImport({
-      imports: [codeSnippetAutoImport],
-    }),
-    astroCodeSnippets(),
-    prefetch(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-    mdx(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), react(), AutoImport({
+    imports: [codeSnippetAutoImport]
+  }), astroCodeSnippets(), prefetch(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), mdx()],
   vite: {
     define: {
-      "process.env.NODE_ENV": `'${process.env.NODE_ENV}'`,
-    },
+      "process.env.NODE_ENV": `'${process.env.NODE_ENV}'`
+    }
   },
   output: "hybrid",
-  adapter: vercel({
-    functionPerRoute: false,
-  }),
+  adapter: cloudflare(),
   markdown: {
-    remarkPlugins: [remarkReadingTime],
+    remarkPlugins: [remarkReadingTime]
   },
-  site: "https://www.thomasledoux.be",
+  site: "https://www.thomasledoux.be"
 });
