3f8ce10139202b0a891e129d9ab83cff2e33963e
Thomas Ledoux <thomas_ledoux@epam.com>
2023-12-14 20:10:04 +0100

    Fix build err

--- astro.config.ts
+++ astro.config.ts
@@ -5,43 +5,53 @@ import vercel from "@astrojs/vercel/serverless";
 import { remarkReadingTime } from "./src/utils/calculate-reading-time.js";
 import react from "@astrojs/react";
 import AutoImport from "astro-auto-import";
-import { astroCodeSnippets, codeSnippetAutoImport } from "./integrations/astro-code-snippets";
+import {
+  astroCodeSnippets,
+  codeSnippetAutoImport,
+} from "./integrations/astro-code-snippets";
 import partytown from "@astrojs/partytown";
-
-import sentry from "@sentry/astro";
-import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), react(), AutoImport({
-    imports: [codeSnippetAutoImport]
-  }), astroCodeSnippets(), partytown({
-    config: {
-      forward: ["dataLayer.push"]
-    }
-  }), mdx(), sentry(), spotlightjs()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+    AutoImport({
+      imports: [codeSnippetAutoImport],
+    }),
+    astroCodeSnippets(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    mdx(),
+  ],
   vite: {
     define: {
-      "process.env.NODE_ENV": `'${process.env.NODE_ENV}'`
-    }
+      "process.env.NODE_ENV": `'${process.env.NODE_ENV}'`,
+    },
   },
   prefetch: {
-    prefetchAll: true
+    prefetchAll: true,
   },
   output: "hybrid",
   adapter: vercel({
     speedInsights: {
-      enabled: true
-    }
+      enabled: true,
+    },
   }),
   markdown: {
-    remarkPlugins: [remarkReadingTime]
+    remarkPlugins: [remarkReadingTime],
   },
-  site: "https://www.thomasledoux.be"
+  site: "https://www.thomasledoux.be",
 });
-console.log(process.env.VERCEL_ANALYTICS_ID, process.env.PUBLIC_VERCEL_ANALYTICS_ID);
+console.log(
+  process.env.VERCEL_ANALYTICS_ID,
+  process.env.PUBLIC_VERCEL_ANALYTICS_ID,
+);
 if (!process.env.VERCEL_ANALYTICS_ID) {
   process.env.VERCEL_ANALYTICS_ID = process.env.PUBLIC_VERCEL_ANALYTICS_ID;
 }
