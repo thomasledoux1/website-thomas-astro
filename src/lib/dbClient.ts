4881b80762a91a4e683586f924d23f136eb659a1
Thomas Ledoux <thomas_ledoux@epam.com>
2023-12-22 14:46:24 +0100

    Fix credential

--- src/lib/dbClient.ts
+++ src/lib/dbClient.ts
@@ -1,8 +1,7 @@
 import { createPool } from "@vercel/postgres";
 
 const client = createPool({
-  connectionString:
-    "postgres://default:BQuKt3c6qMOW@ep-lucky-star-15879815-pooler.eu-central-1.postgres.vercel-storage.com:5432/verceldb",
+  connectionString: import.meta.env.POSTGRES_URL,
 });
 
 export { client };
