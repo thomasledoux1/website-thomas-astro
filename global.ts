70ebc97b120f0fc47620c5b50d0f587399d71029
Thomas Ledoux <thomas_ledoux@epam.com>
2024-01-10 10:18:43 +0100

    Cleanup code with knip

--- global.ts
+++
@@ -1,9 +0,0 @@
-declare global {
-  interface Window {
-    dataLayer: any;
-  }
-}
-
-window.dataLayer = window.dataLayer || {};
-
-export {};
--- src/lib/dbClient.ts
+++ src/lib/dbClient.ts
@@ -1,15 +1,11 @@
 import { createPool } from "@vercel/postgres";
 import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
-import type { InferSelectModel, InferInsertModel } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/vercel-postgres";
 
 export const PageViewsTable = pgTable("page_views", {
   url: text("url").notNull(),
   date: timestamp("date").defaultNow().notNull(),
 });
-
-export type PageView = InferSelectModel<typeof PageViewsTable>;
-export type NewPageView = InferInsertModel<typeof PageViewsTable>;
 
 // Connect to Vercel Postgres
 
