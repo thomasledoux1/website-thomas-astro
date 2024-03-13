06166d1a9c592f44c506d063d9071c8d6ec1cad9
Thomas Ledoux <thomasledoux1@gmail.com>
2024-03-13 09:24:26 +0100

    Revert astro db stuff

--- astro.config.ts
+++ astro.config.ts
@@ -5,35 +5,42 @@ import vercel from "@astrojs/vercel/serverless";
 import { remarkReadingTime } from "./src/utils/calculate-reading-time.js";
 import react from "@astrojs/react";
 import AutoImport from "astro-auto-import";
-import { astroCodeSnippets, codeSnippetAutoImport } from "./integrations/astro-code-snippets";
-
-import db from "@astrojs/db";
+import {
+  astroCodeSnippets,
+  codeSnippetAutoImport,
+} from "./integrations/astro-code-snippets";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), react(), AutoImport({
-    imports: [codeSnippetAutoImport]
-  }), astroCodeSnippets(), mdx(), db()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+    AutoImport({
+      imports: [codeSnippetAutoImport],
+    }),
+    astroCodeSnippets(),
+    mdx(),
+  ],
   vite: {
     define: {
-      "process.env.NODE_ENV": `'${process.env.NODE_ENV}'`
-    }
+      "process.env.NODE_ENV": `'${process.env.NODE_ENV}'`,
+    },
   },
   prefetch: {
-    prefetchAll: true
+    prefetchAll: true,
   },
   output: "hybrid",
   adapter: vercel({
     speedInsights: {
-      enabled: false
-    }
+      enabled: false,
+    },
   }),
   markdown: {
-    remarkPlugins: [remarkReadingTime]
+    remarkPlugins: [remarkReadingTime],
   },
-  site: "https://www.thomasledoux.be"
+  site: "https://www.thomasledoux.be",
 });
 if (!process.env.VERCEL_ANALYTICS_ID) {
   process.env.VERCEL_ANALYTICS_ID = process.env.PUBLIC_VERCEL_ANALYTICS_ID;
--- src/db/seed.ts
+++
@@ -1,8 +0,0 @@
-import { db, PageView } from "astro:db";
-
-export default async function () {
-  await db.insert(PageView).values([
-    { date: new Date(), url: "/" },
-    { date: new Date(new Date().getTime() - 1000), url: "/test" },
-  ]);
-}
