9637f0d16b82a1778cd56d9301f09d7cfaa54aa8
Thomas Ledoux <thomasledoux1@gmail.com>
2024-03-18 19:17:36 +0100

    Fix type errors

--- src/pages/api/migrate-view.ts
+++ src/pages/api/migrate-view.ts
@@ -1,17 +1,18 @@
 export const prerender = false;
 import type { APIRoute } from "astro";
-import { db, PageView } from "astro:db";
+import { db, gt, PageView } from "astro:db";
 import { asc } from "drizzle-orm";
 import { PageViewsTable, client } from "~/lib/dbClient";
 
 export const GET: APIRoute = async () => {
   try {
-    const entries = (
-      await client
-        .select()
-        .from(PageViewsTable)
-        .orderBy(asc(PageViewsTable.date))
-    ).slice(24976);
+    const entries = await client
+      .select()
+      .from(PageViewsTable)
+      .orderBy(asc(PageViewsTable.date))
+      // @ts-expect-error
+      .where(gt(PageViewsTable.date, new Date("2024-03-16")));
+    console.log(entries);
     for (const entry of entries) {
       await db.insert(PageView).values({ url: entry.url, date: entry.date });
     }
