490ea76f5a50dc4a3189f807222cc0d0083177f1
Thomas Ledoux <thomasledoux1@gmail.com>
2024-05-15 16:57:33 +0200

    Use expressive-code for code blocks + cleanups

--- astro.config.ts
+++ astro.config.ts
@@ -4,38 +4,38 @@ import mdx from "@astrojs/mdx";
 import vercel from "@astrojs/vercel/serverless";
 import { remarkReadingTime } from "./src/utils/calculate-reading-time.js";
 import react from "@astrojs/react";
-import AutoImport from "astro-auto-import";
-import { astroCodeSnippets, codeSnippetAutoImport } from "./integrations/astro-code-snippets";
 import db from "@astrojs/db";
-
 import sitemap from "@astrojs/sitemap";
+
+import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), react(), AutoImport({
-    imports: [codeSnippetAutoImport]
-  }), astroCodeSnippets(), mdx(), db(), sitemap()],
-  vite: {
-    define: {
-      "process.env.NODE_ENV": `'${process.env.NODE_ENV}'`
-    }
-  },
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+    expressiveCode({
+      frames: {
+        showCopyToClipboardButton: true,
+      },
+    }),
+    mdx(),
+    db(),
+    sitemap(),
+  ],
   prefetch: {
-    prefetchAll: true
+    prefetchAll: true,
   },
   output: "hybrid",
   adapter: vercel({
     speedInsights: {
-      enabled: false
-    }
+      enabled: false,
+    },
   }),
   markdown: {
-    remarkPlugins: [remarkReadingTime]
+    remarkPlugins: [remarkReadingTime],
   },
-  site: "https://www.thomasledoux.be"
+  site: "https://www.thomasledoux.be",
 });
-if (!process.env.VERCEL_ANALYTICS_ID) {
-  process.env.VERCEL_ANALYTICS_ID = process.env.PUBLIC_VERCEL_ANALYTICS_ID;
-}
