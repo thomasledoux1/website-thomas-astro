ddc5a7257ab6057d1ebb06a40e16988c6c8843bf
Thomas Ledoux <thomasledoux1@gmail.com>
2025-02-28 10:49:46 +0100

    no longer use images for blogs

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,15 +3,12 @@ import { glob } from "astro/loaders";
 
 const blog = defineCollection({
   loader: glob({ pattern: "**/*.mdx", base: "./src/data/blog" }),
-  schema: ({ image }) =>
+  schema: () =>
     z.object({
       title: z.string(),
       tags: z.array(z.string()),
       date: z.string().datetime(),
-      image: image(),
-      imageAlt: z.string(),
       draft: z.boolean().optional(),
-      containImage: z.boolean().optional(),
     }),
 });
 
